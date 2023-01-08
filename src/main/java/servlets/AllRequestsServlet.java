da29d7a9bb1fe7d0df0e811ea663cd6a370b32ca
pepya4ka <ndrozdov9@gmail.com>
2023-01-08 19:26:15 +0300

    add hw01

---
+++ src/main/java/servlets/AllRequestsServlet.java
@@ -0,0 +1,24 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AllRequestsServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String message = request.getParameter("key");
+
+        response.setContentType("text/html;charset=utf-8");
+
+        if (message == null || message.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        } else {
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+        response.getWriter().println(message);
+    }
+}
